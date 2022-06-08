7bb971b72256f12c7f03c06b3dbbdfa3dd81a71e
Ricky Medina <rickmedina85@gmail.com>
2022-06-08 19:03:23 -0400

    Solve exercise #5

--- js-exercises/javascript-exercises/05_sumAll/sumAll.js
+++ js-exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (start < 0 || end < 0 ||
+        typeof start !== 'number' || typeof end !== 'number')
+    {
+        return 'ERROR'
+    }
+
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+
+    let res = start;
+    let second_operand = start;
+    while (second_operand < end) {
+        second_operand++;
+        res += second_operand;
+    }
+    return res;
 };
 
 // Do not edit below this line
