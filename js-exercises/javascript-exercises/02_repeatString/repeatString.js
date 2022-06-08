4eda729204bf692616a713e79c8f3fee9a1b3423
Ricky Medina <rickmedina85@gmail.com>
2022-06-08 18:04:50 -0400

    Solve exercise #2

--- js-exercises/javascript-exercises/02_repeatString/repeatString.js
+++ js-exercises/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
+    let new_str = '';
+    for (let i = 0; i < num; i++) {
+        new_str = new_str.concat(string)
+    };
+    return new_str;
 };
 
 // Do not edit below this line
