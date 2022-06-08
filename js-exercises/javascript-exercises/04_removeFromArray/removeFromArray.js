68dec4f42ddd0f647d0faef376a7a246817316e2
Ricky Medina <rickmedina85@gmail.com>
2022-06-08 18:41:28 -0400

    Solve exercise #4

--- js-exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ js-exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elems) {
+    for (const elem of elems) {
+        const elemIdx = arr.indexOf(elem)
+        if (elemIdx === -1) continue
+        arr.splice(elemIdx, 1)
+    }
+    return arr
 
 };
 
