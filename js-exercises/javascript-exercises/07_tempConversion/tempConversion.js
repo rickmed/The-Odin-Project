bcfc092d1dcac69515edf5c8a57eeb31dce2f200
Ricky Medina <rickmedina85@gmail.com>
2022-06-08 19:30:19 -0400

    Solve exercise #7

--- js-exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ js-exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f_temp) {
+    let res = (f_temp - 32) * 5/9;
+    return Math.round(res * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c_temp) {
+    let res = c_temp * 9/5 + 32;
+    return Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
