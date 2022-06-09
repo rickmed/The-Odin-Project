a7da099bb062e2f28f8cc1f0818f339d5e93ac62
Ricky Medina <rickmedina85@gmail.com>
2022-06-09 13:23:59 -0400

    Add ui to rps game

--- js-exercises/rock-paper-scissors/script.js
+++ js-exercises/rock-paper-scissors/script.js
@@ -11,37 +11,61 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
-    let winner;
-
-    if (playerSelection === computerSelection) {
-        return `It's a draw between ${playerSelection}s`
-    }
-
-    if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+    const result = playerSelection === computerSelection ? 'draw' :
+       (playerSelection === 'rock' && computerSelection === 'scissors' ||
         playerSelection === 'scissors' && computerSelection === 'paper' ||
-        playerSelection === 'paper' && computerSelection === 'rock'
-    ) {
-        winner = 'player'
-    } else {
-        winner = 'computer'
-    }
-
-    const result = winner === 'player' ?
-        `You win! ${playerSelection} beats ${computerSelection} ` :
-        `You lose! ${computerSelection} beats ${playerSelection} `
+        playerSelection === 'paper' && computerSelection === 'rock') ? 'player' :
+    'computer'
 
     return result
 }
 
-function game() {
-    const playerSelection = prompt('Your move:').toLowerCase()
-    console.log(playRound(playerSelection, computerPlay()))
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+let score = {
+    computer: 0,
+    player: 0
 }
 
-console.log(`Let's play 3 rounds!`);
-for (let i = 0; i < 3; i++) {
-    game()
-}
-console.log('game over')
+for (button of $$(`.selection`)) {
 
+    button.addEventListener(`click`, e => {
+        const playerSelection = e.target.id;
+        const computerSelection = computerPlay();
+        const round_winner = playRound(playerSelection, computerSelection);
+
+        // update score
+        const winner_score = score[round_winner];
+        if (winner_score < 5) {
+            score[round_winner] = winner_score + 1
+        }
+
+        const score_text =
+            `Player: ${score.player} - Computer: ${score.computer}`;
+        $(`#score`).textContent = score_text;
+
+        const round_result_msg = round_winner === 'draw' ?
+            `It's a draw between ${playerSelection}s` :
+                round_winner === 'player' ?
+            `You win! ${playerSelection} beats ${computerSelection} ` :
+            `You lose! ${computerSelection} beats ${playerSelection} `;
+
+        // Append round result
+        let result_ui = document.createElement(`div`);
+        result_ui.textContent = round_result_msg;
+        $(`body`).appendChild(result_ui);
+
+        console.log(winner_score);
+        if (winner_score === 5) {
+            let game_winner_ui = document.createElement(`h3`);
+            result_ui.textContent = `${round_winner} wins!`;
+            $(`body`).appendChild(result_ui);
+        }
+
+    })
+}
+
+console.clear();
 
