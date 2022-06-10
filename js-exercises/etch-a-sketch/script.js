3d4af6278893f21dbb7e42a640227cd9c43ad067
Ricky Medina <rickmedina85@gmail.com>
2022-06-10 15:41:44 -0400

    Complete etch-a-sketch task

--- js-exercises/etch-a-sketch/script.js
+++ js-exercises/etch-a-sketch/script.js
@@ -1,13 +1,37 @@
 // console.clear();
 const $ = document.querySelector.bind(document)
 
-for (let i = 0; i < 16 * 16; i++) {
-    const elem = document.createElement('div');
-    elem.classList.add('square')
-    elem.id = `${i}`;
-    elem.width = '10px';
-    elem.style.border = '1px solid red';
-    elem.textContent = i;
-    elem.style.fontSize = '12px';
-    $('#container').appendChild(elem);
+let grid = $('grid');
+
+$('#new_grid').addEventListener('click', e => {
+    const answer = parseInt(prompt('Grid size?'));
+    if (answer > 100) {
+        alert('Sorry, 100 is the maximum');
+        return
+    }
+    change_grid(answer);
+})
+
+change_grid();
+
+function change_grid(size_size = 16) {
+    grid.style['grid-template-columns'] = `repeat(${size_size}, 1fr)`;
+    const cells = create_cells(size_size);
+    grid.replaceChildren(...cells);
+
+    function create_cells(n_squares) {
+        const cells = [...Array(n_squares ** 2)].map( _ => {
+            return create_cell()
+        })
+        return cells
+    }
+
+    function create_cell() {
+        const cell = document.createElement('cell');
+        cell.style.border = '1px solid black';
+        cell.addEventListener('mouseover', e => {
+            cell.style.backgroundColor = 'yellow';
+        })
+        return cell
+    }
 }
