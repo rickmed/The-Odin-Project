986165fd6fb70b4ebc61a0b621f3c0b50c6bb44e
Ricky Medina <rickmedina85@gmail.com>
2022-06-28 11:46:27 -0400

    Add basic project code structure

---
+++ calculator/script.js
@@ -0,0 +1,24 @@
+console.clear();
+
+function operate (operation, op1, op2) {
+    // body
+}
+
+
+function add (x, y) {
+    // body
+}
+
+
+function subtract (x, y) {
+    // body
+}
+
+function multiply (x, y) {
+    // body
+}
+
+function divide (x, y) {
+    // body
+}
+
